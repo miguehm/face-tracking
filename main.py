084900fa78204cdcc6e282cdf4af2dd451c52494
miguehm <miguel.hernandezmor@alumno.buap.mx>
2024-05-14 02:49:51 -0600

    add some test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from send_coords import send_coords
-from face_detection import face_detection
+from face_webcam import face_detection
 
 def main():
-    error_x, error_y = face_detection()
-    send_coords(error_x, error_y)
+    face_detection()
+    # send_coords(error_x, error_y)
 
 if __name__ == '__main__':
     main()
